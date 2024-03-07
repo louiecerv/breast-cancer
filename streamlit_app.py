e76e58c9febed75ca77bbb0346f187e938b8a193
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-07 11:49:58 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ def display_form1():
     form1.text(text)
 
     form1.header('Description')
-    form1.image('iris_flower.jpg', caption="The Iris Plant", use_column_width=True)
+    form1.image('breast-cancer.png', caption="The Breast Cancer Dataset in Sci-kit Learn", use_column_width=True)
     text = """The iris is a beautiful and diverse flowering plant genus, 
     boasting over 310 recognized species. These plants are known for their 
     stunning blooms, which come in a wide range of colors, including purple, 
